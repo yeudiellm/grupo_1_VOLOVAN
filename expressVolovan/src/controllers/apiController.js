72310f4a3c589dec086b9fa387c9e8b6aadf893a
Luis Núñez <njluisangel@gmail.com>
2021-10-27 23:32:21 -0500

    restructured apis json

--- expressVolovan/src/controllers/apiController.js
+++ expressVolovan/src/controllers/apiController.js
@@ -4,13 +4,21 @@ const DB = require('../../src/database/models/Productos')
 const controller = {
     users: (req, res) => {
         return res.json({
-            count: 1,
-            users: {
-                id: 1,
-                name: 'Luis',
-                email: 'luis@correo.com',
-                detail: 'url para obtener el detalle'
-            }
+            count: 2,
+            users: [
+                {
+                    id: 1,
+                    name: 'Luis',
+                    email: 'luis@correo.com',
+                    detail: 'url para obtener el detalle'
+                },
+                {
+                    id: 2,
+                    name: 'Pedro',
+                    email: 'pedro@correo.com',
+                    detail: 'url para obtener el detalle'
+                },
+            ],
         });
     },
     userDetail: (req, res) => {
@@ -23,7 +31,7 @@ const controller = {
     },
     products: (req, res) => {
         return res.json({
-            count: '30',
+            count: '2',
             countByCategory: {
                 salados: {
                     totalProducts: '6'
@@ -32,13 +40,22 @@ const controller = {
                     totalProducts: '6'
                 },
             },
-            products: {
-                id: 1,
-                name: 'Volovan de Jamon',
-                description: 'Hojaldre relleno con jamon, queso amarrillo y salsa chipotle. ',
-                // categories: [],
-                detail: 'url para obtener el detalle',
-            }
+            products: [
+                {
+                    id: '1',
+                    name: 'volovan de jamon',
+                    description: 'un volovan que tiene jamon',
+                    category: 'salados',
+                    details: 'products/detail/1',
+                },
+                {
+                    id: '2',
+                    name: 'volovan de piña',
+                    description: 'un volovan que tiene piña',
+                    category: 'dulce',
+                    details: 'products/detail/2',
+                },
+            ]
         })
     },
     productDetail: (req, res) => {
@@ -48,7 +65,7 @@ const controller = {
             product_price: '13',
             product_description: 'Hojaldre relleno con jamon, queso amarillo y salsa chipotle. ',
             category: 'salados',
-            image: 'volovan-jamon.jpg',
+            image: 'images/products/volovan-jamon.jpg',
         })
     }
 }
